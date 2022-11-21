273d7f0bc5efdd2f19452dd6da3ac298e403c4f4
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-21 14:35:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats= ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name){
+    cats;
+    console.log(cats.push('Ralph'));
+}
+function destructivelyPrependCat(name){
+    cats;
+    console.log(cats.unshift(name));
+}
+function destructivelyRemoveLastCat(name){
+    cats;
+    console.log(cats.pop(name));
+}
+function destructivelyRemoveFirstCat(name){
+    cats;
+    console.log(cats.shift(name));
+}
+function appendCat(name){
+    cats;
+    const addedCat = [...cats, name];
+    return addedCat;
+}
+function prependCat(name){
+    cats;
+    const addedCat = [name, ...cats];
+    return addedCat;
+}
+function removeLastCat(name){
+    cats;
+    const addedCat = cats.slice(0,2);
+    return addedCat;
+}
+function removeFirstCat(name){
+    cats;
+    const addedCat = cats.slice(1);
+    return addedCat;
+}
